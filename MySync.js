c5f52522b510088559eee1f6d2b437a9433eb243
lyolich <pes_dukk@proton.me>
2026-04-08 17:03:09 +0300

    Update MySync.js

--- MySync.js
+++ MySync.js
@@ -25,7 +25,7 @@
     // --- НАЛАШТУВАННЯ ---
     var MPC_PATH = 'C:\\Program Files\\MPC-BE\\mpc-be64.exe'; // Вкажіть правильний шлях до вашого плеєру!!!
     var NODE_EXE_PATH = 'C:\\Program Files\\nodejs\\node.exe'; // Вкажіть правильний шлях до вашого node.exe !!!
-    var PROXY_SCRIPT_PATH = 'X:\\Lampa\\SCRIPTS\\mpc-proxy.js'; // Вкажіть правильний шлях до вашого проксі !!!
+    var PROXY_SCRIPT_PATH = 'E:\\mpc-proxy.js'; // Вкажіть правильний шлях до вашого проксі !!!
     var PROXY_URL = 'http://localhost:8080';
     var MAX_FAILS = 1;
 
